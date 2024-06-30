5efa2e3a4a6d1231b189b788ff0ddcdb7e7a0a76
Adaliz Membreno <adalizitachi11@gmail.com>
2024-06-29 19:29:35 -0600

    add player video from local

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 import 'package:toktik/presentation/widgets/video_player/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
@@ -17,6 +18,12 @@ class VideoScrollableView extends StatelessWidget {
           final videoPost = videos[index];
 
           return Stack(children: [
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                videoTitle: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+              ),
+            ),
             Positioned(
                 bottom: 40, right: 20, child: VideoButtons(video: videoPost))
           ]);
